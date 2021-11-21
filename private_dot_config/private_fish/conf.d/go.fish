31af08e5481d476cf30d59009992ba585ed5a450
Joel Gerber <joel.gerber@molex.com>
2021-11-21 11:09:37 -0500

    switch to desired go version if installed

--- private_dot_config/private_fish/conf.d/go.fish
+++ private_dot_config/private_fish/conf.d/go.fish
@@ -1,1 +1,8 @@
+set _desired_go_version 1.17.3
+
 fish_add_path (go env GOPATH)/bin
+
+if which go$_desired_go_version >/dev/null 2>&1
+    set -x GOROOT (go$_desired_go_version env GOROOT)
+    fish_add_path $GOROOT/bin
+end
