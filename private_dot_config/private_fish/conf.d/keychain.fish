6e89c474fc8cc5d04f5f4471bc4e547f495ff8c5
Joel Gerber <joel.gerber@molex.com>
2022-03-19 17:30:40 -0400

    only engage keychain when it is installed

--- private_dot_config/private_fish/conf.d/keychain.fish
+++ private_dot_config/private_fish/conf.d/keychain.fish
@@ -1,1 +1,3 @@
-status is-interactive; and eval (keychain --agents gpg,ssh --eval 2>/dev/null)
+if test (which keychain)
+    status is-interactive; and eval (keychain --agents gpg,ssh --eval 2>/dev/null)
+end
