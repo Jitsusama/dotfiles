5aa6f0a4e6e957b4b9c74f84c5d6f38335708c20
Joel Gerber <joel.gerber@molex.com>
2022-03-19 17:33:54 -0400

    only configure omf when it is installed

--- private_dot_config/private_fish/conf.d/omf.fish
+++ private_dot_config/private_fish/conf.d/omf.fish
@@ -3,5 +3,7 @@ set -q XDG_DATA_HOME
 and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
 or set -gx OMF_PATH "$HOME/.local/share/omf"
 
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
+if test -d $OMF_PATH
+    # Load Oh My Fish configuration.
+    source $OMF_PATH/init.fish
+end
