bc67a2e676334ea77bb898b2f229167d7653d014
Joel Gerber <joel.gerber@molex.com>
2022-11-13 13:52:44 -0500

    Move p10k git status to right 2nd line

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -34,7 +34,6 @@
     # =========================[ Line #1 ]=========================
     os_icon                 # os identifier
     dir                     # current directory
-    vcs                     # git status
     # =========================[ Line #2 ]=========================
     newline                 # \n
     prompt_char             # prompt symbol
@@ -105,6 +104,7 @@
     # time                  # current time
     # =========================[ Line #2 ]=========================
     newline
+    vcs                     # git status
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
